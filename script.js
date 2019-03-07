19e0083b2ca6c90b39a5c96c6a37b49567577ce4
jqF1erey <43240454+jqF1erey@users.noreply.github.com>
2019-03-07 08:29:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".block").click(function(){
+    $(".bird").slideToggle();
+});
+
+$(".toggle").click(function(){
+    $(".gone").toggle();
+    $(".tails").toggle();
+});
+
+$(".run").mouseover(function(){
+    $(".run").fadeOut();
+});
+
+
+
+
+
+
+
